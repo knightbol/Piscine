a2a991c92bd1ecec812f2d522b8a15239bd22a71
Rodrigo Caixinha <rcaixinha@hotmail.com>
2025-03-26 17:59:39 +0000

    Uploaded from home

--- C04/ex05/ft_atoi_base.c
+++ C04/ex05/ft_atoi_base.c
@@ -1,3 +1,11 @@
+int	ft_is_whitespace(char str)
+{
+	if (str == ' ' || str == '\n' 
+		|| str == '\t' || str == '\f' 
+		|| str == '\v' || str == '\r')
+		return (1);
+}
+
 int	ft_is_valid_base(char *base)
 {
 	char	freq[256] = {0};
@@ -17,9 +25,26 @@ int	ft_is_valid_base(char *base)
 	}
 	return (1);
 }
-int	ft_atoi_base(char *str, char  *base)
+int	ft_atoi_base(char *str, char *base)
 {
+	int	result;
+	int	digit;
+
+	result = 0;
+	digit = 0;
 	if (!str || !ft_is_valid_base(base))
 		return (0);
-	if 
+	while (ft_is_whitespace(*str))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign++;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result += (*str - 48);
+		result *= 10;
+	}
 }
